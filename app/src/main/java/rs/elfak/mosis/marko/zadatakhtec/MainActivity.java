525357b5b309cd90dfd5bdf4b4b6518b602a1289
markopesterac <markopesterac@elfak.rs>
2018-04-01 16:26:10 +0200

    Start2

--- app/src/main/java/rs/elfak/mosis/marko/zadatakhtec/MainActivity.java
+++ app/src/main/java/rs/elfak/mosis/marko/zadatakhtec/MainActivity.java
@@ -2,12 +2,28 @@ package rs.elfak.mosis.marko.zadatakhtec;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Row> rows;
+    private RowAdapter rowAdapter;
+    ListView listview;
+    List<Row> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listview=(ListView)findViewById(R.id.list_view);
+        list= new ArrayList<>();
+        rowAdapter = new RowAdapter(this, list);
+
+
+
     }
 }
---
+++ app/src/main/java/rs/elfak/mosis/marko/zadatakhtec/Row.java
@@ -0,0 +1,22 @@
+package rs.elfak.mosis.marko.zadatakhtec;
+
+/**
+ * Created by Marko on 3/29/2018.
+ */
+
+public class Row {
+    public String image;
+    public String title;
+    public String description;
+
+    public Row(){}
+
+    public String getImage() {return image;}
+    public String getTitle() {return title;}
+    public String getDescription() {return description;}
+
+    public void setImage(String img) {image=img;}
+    public void setTitle(String tit) {title=tit;}
+    public void setDescription(String desc) {description=desc;}
+
+}
