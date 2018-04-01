03df8029aa8a5d53cf09b1b9d370f5c83ff6db2f
markopesterac <markopesterac@elfak.rs>
2018-04-01 17:05:52 +0200

    WebRequest&GetRows

--- app/src/main/java/rs/elfak/mosis/marko/zadatakhtec/MainActivity.java
+++ app/src/main/java/rs/elfak/mosis/marko/zadatakhtec/MainActivity.java
@@ -1,29 +1,89 @@
 package rs.elfak.mosis.marko.zadatakhtec;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Row> rows;
     private RowAdapter rowAdapter;
-    ListView listview;
     List<Row> list;
+
+    ListView listView;
+
+
+    private static String url="https://raw.githubusercontent.com/danieloskarsson/mobile-coding-exercise/master/items.json";
+
+    // JSON Node names
+    private static final String TAG_IMAGE = "image";
+    private static final String TAG_TITLE = "title";
+    private static final String TAG_DESCRIPTION = "description";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listview=(ListView)findViewById(R.id.list_view);
-        list= new ArrayList<>();
-        rowAdapter = new RowAdapter(this, list);
+        listView=(ListView)findViewById(R.id.list_view);
+
+        //Callig async task to get JSON
+        //new GetRows().execute();
+
+    }
+
+    private class GetRows extends AsyncTask<Void,Void,Void>
+    {
+        //Hashmap for listView
+        ArrayList<HashMap<String,String>> rowList;
+        ProgressDialog proDialog;
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            proDialog = new ProgressDialog(MainActivity.this);
+            proDialog.setMessage("Please wait...");
+            proDialog.setCancelable(false);
+            proDialog.show();
+        }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+            WebRequest webreq = new WebRequest();
+            // Making a request to url and getting response
+            String jsonStr = webreq.makeWebServiceCall(url);
+            Log.d("Response: ", "> " + jsonStr);
+            rowList = ParseJSON(jsonStr);
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void requestresult) {
+            super.onPostExecute(requestresult);
+            // Dismiss the progress dialog
+            if (proDialog.isShowing())
+                proDialog.dismiss();
+            /**
+             * Updating received data from JSON into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, rowList,
+                    R.layout.list_item, new String[]{TAG_TITLE,TAG_DESCRIPTION },
+                    new int[]{R.id.row_title, R.id.row_description});
+            listView.setAdapter(adapter);
+        }
 
 
 
     }
+
 }
---
+++ app/src/main/java/rs/elfak/mosis/marko/zadatakhtec/WebRequest.java
@@ -0,0 +1,77 @@
+package rs.elfak.mosis.marko.zadatakhtec;
+
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import static com.bumptech.glide.gifdecoder.GifHeaderParser.TAG;
+
+/**
+ * Created by Marko on 4/1/2018.
+ */
+
+public class WebRequest {
+
+    public WebRequest() {}
+
+    public String makeWebServiceCall(String urladdress)
+    {
+        String response=null;
+        try{
+            URL url=new URL(urladdress);
+            HttpsURLConnection con=(HttpsURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+
+            InputStream in=new BufferedInputStream(con.getInputStream());
+            response=convertStreamToString(in);
+
+        }
+        catch (MalformedURLException e) {
+            Log.e(TAG, "MalformedURLException: " + e.getMessage());
+        } catch (ProtocolException e) {
+            Log.e(TAG, "ProtocolException: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "IOException: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+
+        return response;
+    }
+
+    public String convertStreamToString(InputStream in)
+    {
+        BufferedReader reader=new BufferedReader(new InputStreamReader(in));
+        StringBuilder sb=new StringBuilder();
+
+        String line;
+
+        try
+        {
+            while((line=reader.readLine())!=null)
+            {
+                sb.append(line).append('\n');
+            }
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+
+}
